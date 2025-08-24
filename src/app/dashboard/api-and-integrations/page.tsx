69f2ba52756b9ebb74c854de7f8d6611ee5a0d28
LuigiSotoF <luigi.soto@dropkiller.com>
2025-08-24 01:15:25 -0500

    feat:replace font for inter

--- src/app/dashboard/api-and-integrations/page.tsx
+++ src/app/dashboard/api-and-integrations/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function UsersPage() {
   return (
     <div className='flex flex-col gap-4 items-center justify-center'>
-      <h1 className='text-2xl font-bold'>Usuarios</h1>
+      <h1 className='text-2xl font-semibold'>Usuarios</h1>
       <div className='flex flex-col gap-2'>
-        <h2 className='text-lg font-bold'>Usuarios</h2>
+                  <h2 className='text-lg font-semibold'>Usuarios</h2>
         <p className='text-sm text-gray-500'>Usuarios</p>
       </div>
     </div>
